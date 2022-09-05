171902002784877b324e2d82e06aa03399fc9991
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-05 19:57:56 -0400

    finished homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import LogoTitle from "../../assets/images/logo-s.png"
+import LogoTitle from "../../assets/images/logo.png"
 import { Link } from "react-router-dom"
 import './index.scss';
 import AnimatedLetters from "../AnimatedLetters";
@@ -8,8 +8,8 @@ import { useEffect, useState } from "react";
 const Home = () => {
 
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['w','l','a','d','y','s,']
-  const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
+  const nameArray = ['w','l','a','d','y','s!']
+  const jobArray = ['W','e','b',' ','D','e','v','e','l','o','p','e','r','_']
 
 
   useEffect(() => {
@@ -38,17 +38,18 @@ const Home = () => {
         <span className={`${letterClass} _19`}>e</span>
         <span className={`${letterClass} _20`}>r</span>
         <span className={`${letterClass} _21`}>e,</span>
-        <br />
-        <span className={`${letterClass} _22`}>I</span>
-        <span className={`${letterClass} _23`}>'m</span>
-        <img src={LogoTitle} alt="developer"/>
+        <span className={`${letterClass} _22`}> </span>
+        <span className={`${letterClass} _23`}>I</span>
+        <span className={`${letterClass} _24`}>'m</span>
+        <span className={`${letterClass} _25`}> </span>
+        <span className={`${letterClass} _26`}>G</span>
         <AnimatedLetters letterClass={letterClass}
         strArray={nameArray}
-        idx={24}/>
+        idx={26}/>
         <br />
         <AnimatedLetters letterClass={letterClass}
         strArray={jobArray}
-        idx={30}/>
+        idx={32}/>
         </h1>
         <h2>Inspiring Full Stack Developer</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
