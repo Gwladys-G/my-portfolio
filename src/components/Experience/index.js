d4e876362304b784c71becd579407ea18ccbb19a
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-07 19:48:11 -0400

    finished acceptable responsiveness

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,12 +1,49 @@
 import Loader from 'react-loaders'
 import './index.scss'
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
+import Pdf from './Gwladys_Engels_Aug_2022.pdf';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 
 const Experience = () => {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    let timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 2000)
+
+    return () => {
+        clearTimeout(timeoutId)
+    }
+  }, [])
+
+
+
   return (
     <>
-    <div className='container experience-page'>
-    </div>
-    <Loader type="ball-spin-fade-loader"/>
+      <div className='container experience-page'>
+        <h1 className='page-title'>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={"Background".split("")}
+              idx={15}
+            />
+        </h1>
+        <div className="Download-Resume">
+            <button><a href = {Pdf} target = "_blank" rel="noreferrer">Download My Resume</a></button>
+        </div>
+        <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/gwladysengels/' className='Linkedin-bck'>
+              <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+        </a>
+
+     </div>
+      <Loader type="ball-spin-fade-loader"/>
     </>
   )
 }
