b6662ee7d07fd78c82d5ae83d866b94aea21bc18
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-07 14:48:41 -0400

    made about section responsive

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,16 +17,16 @@ const Sidebar = () => {
           </Link>
         </h4>
         <nav className={showNav? 'mobile-show' : ''}>
-          <NavLink exact="true" activeclassname="active" to="/">
+          <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
             <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
           </NavLink>
-          <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
             <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
           </NavLink>
-          <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+          <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio" onClick={() => setShowNav(false)}>
             <FontAwesomeIcon icon={faEye} color="#4d4d4e" />
           </NavLink>
-          <NavLink exact="true" activeclassname="active" className="experience-link" to="/experience">
+          <NavLink exact="true" activeclassname="active" className="experience-link" to="/experience" onClick={() => setShowNav(false)}>
             <FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
           </NavLink>
           {/* <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
