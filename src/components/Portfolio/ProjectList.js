80b8017935b788997620eb3dde958637c4d9a07d
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-07 16:09:59 -0400

    continued responsiveness

--- src/components/Portfolio/ProjectList.js
+++ src/components/Portfolio/ProjectList.js
@@ -9,31 +9,31 @@ export default function ProjectList() {
   return (
     <div className="images-container">
     {
-        portfolioData.portfolio.map((project, idx) => {
-            return (
-                <div className="image-box" key={idx}>
-                  {/* <img src="https://via.placeholder.com/50" alt="snapshot" className="portfolio-image"/> */}
-                  <img src={project.cover} alt="snapshot" className="portfolio-image"/>
-                  <div className="content">
-                      <p className="title">{project.title}</p>
-                      <h4 className="description">{project.skills.join(", ")}</h4>
-                      <div className='wrapper-cta'>
-                      <button
-                          className="btn"
-                          onClick={() => window.open(project.url)}
-                      >View
-                      </button>
-                      <FontAwesomeIcon
-                        icon={faGithub}
-                        className="github"
-                        onClick={() => window.open(project.github)}
-                        color='#4d4d4e'
-                      />
-                      </div>
+      portfolioData.portfolio.map((project, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              {/* <img src="https://via.placeholder.com/50" alt="snapshot" className="portfolio-image"/> */}
+              <img src={project.cover} alt="snapshot" className="portfolio-image"/>
+              <div className="content">
+                  <p className="title">{project.title}</p>
+                  <h4 className="description">{project.skills.join(", ")}</h4>
+                  <div className='wrapper-cta'>
+                  <button
+                      className="btn"
+                      onClick={() => window.open(project.url)}
+                  >View
+                    </button>
+                    <FontAwesomeIcon
+                      icon={faGithub}
+                      className="github"
+                      onClick={() => window.open(project.github)}
+                      color='#4d4d4e'
+                    />
                   </div>
-                </div>
-            )
-        })
+              </div>
+            </div>
+          )
+      })
     }
   </div>
   )
