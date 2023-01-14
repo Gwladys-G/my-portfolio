0f22d77e047f04a9236ad851deef45a7f731f9c9
“GwladysEngels” <gengels@trimoz.com>
2023-01-14 15:06:04 +0100

    updated about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import AnimatedLetters from "../AnimatedLetters"
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faTrack } from '@fortawesome/free-solid-svg-icons'
-import {faReact, faHtml5, faCss3, faJsSquare, faGitAlt, faNodeJs } from '@fortawesome/free-brands-svg-icons'
+import {faReact, faHtml5, faCss3, faJsSquare, faGitAlt, faVuejs } from '@fortawesome/free-brands-svg-icons'
 import Loader from "react-loaders";
 import { Link } from "react-router-dom";
 
@@ -33,17 +33,13 @@ const About = () => {
           />
         </h1>
         <p>
-          After working in Product Management and
-          Product Operations in the tech industry for several
-          years, i have decided to pursue my interest and excitment towards the technical aspects of that field; Web and Software Development.
+          After working in Product Management and Product Operations in the tech industry for several years, i have decided to pursue my interest and excitement towards Web and Software Development.
         </p>
+        <p>My technology skillsets currently include <span style={{fontWeight: 'bold'}}>Ruby On Rails</span>,<span style={{fontWeight: 'bold'}}> Javascript</span>,<span style={{fontWeight: 'bold'}}> VueJS</span> and <span style={{fontWeight: 'bold'}}>ReactJS</span>.</p>
+        <p>I am actively practising <span style={{fontStyle: 'italic'}}>TypeScript</span> and <span style={{fontStyle: 'italic'}}>Express - NodeJs</span> and will update my <span>{<Link to="/portfolio" style = {{textDecoration: 'none', color: "#694F5D"}}>Portfolio</Link>}</span> as soon as I have projects up and running !</p>
         <p>
-          My technology skillsets currently include <span style={{fontWeight: 'bold'}}>Ruby On Rails</span>,<span style={{fontWeight: 'bold'}}> Javascript</span>, and <span style={{fontWeight: 'bold'}}>ReactJS</span>.
-          I am actively practising <span style={{fontStyle: 'italic'}}>TypeScript</span> and <span style={{fontStyle: 'italic'}}>Express - NodeJs</span> and will update my <span style={{textDecoration: 'none',color: 'green'}} >{<Link to="/portfolio">Portfolio</Link>}</span> as soon as I have projects up and running !
-        </p>
-        <p>
-          Additionaly, I have both my Professional Scrum Master I (PSM I) and Professional Scrum Product Owner (PSPO I)
-          certifications and enjoy working in Agile environments 😉.
+          Additionally, I have both my Professional Scrum Master I (PSM I) and Professional Scrum Product Owner (PSPO I)
+          certifications.
         </p>
       </div>
 
@@ -62,7 +58,7 @@ const About = () => {
             <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
           </div>
           <div className="face5">
-            <FontAwesomeIcon icon={faNodeJs} color="#7FC728" />
+            <FontAwesomeIcon icon={faVuejs} color="#7FC728" />
           </div>
           <div className="face6">
             <FontAwesomeIcon icon={faGitAlt} color="red" />
