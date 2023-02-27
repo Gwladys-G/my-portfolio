e4f967bd9de96f9e7a097874cef3ac11897a708e
GwladysE <gengels@trimoz.com>
2023-02-27 18:59:45 -0500

    updated resume

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -2,7 +2,7 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from "../AnimatedLetters";
 import { useEffect, useState } from "react";
-import Pdf from './Gwladys_Engels_Oct_2022.pdf';
+import Pdf from './Gwladys_Engels_Feb_2023.pdf';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
