0d351aa7486cb095bcd5672459551b3e98b20a39
GwladysE <gengels@trimoz.com>
2023-03-01 13:24:06 -0500

    expanded logo on hover

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -10,25 +10,35 @@ const Project = ({ project }) =>{
     <div className="image-box">
       <img src={project.cover} alt="snapshot" className="portfolio-image"/>
       <div className="content">
-          <p className="title">{project.title}</p>
-          <h4 className="description" style={{ display: 'inline-block' }}>
-            {project.skills.map((skill) => (
-              <img src={skillIcons[skill]} alt={skill} style={{ width: '10%', height: '15%' }} />
-            ))}
-          </h4>
-          <div className='wrapper-cta'>
+        <p className="title">{project.title}</p>
+        <h4 className="description" style={{ display: 'flex', alignItems: 'baseline' }}>
+          {project.skills.map((skill) => (
+            <img
+            src={skillIcons[skill]}
+            alt={skill}
+            style={{
+              width: '10%',
+              height: '15%',
+              transition: 'transform 0.2s',
+              margin: 'auto 5px auto 5px'
+            }}
+            className="skill-icon"
+          />
+          ))}
+        </h4>
+        <div className='wrapper-cta'>
           <button
-              className="btn"
-              onClick={() => window.open(project.url)}
+            className="btn"
+            onClick={() => window.open(project.url)}
           >View
-            </button>
-            <FontAwesomeIcon
-              icon={faGithub}
-              className="github"
-              onClick={() => window.open(project.github)}
-              color='#4d4d4e'
-            />
-          </div>
+          </button>
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="github"
+            onClick={() => window.open(project.github)}
+            color='#4d4d4e'
+          />
+        </div>
       </div>
     </div>
   )
