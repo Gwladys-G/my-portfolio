1e360232441e552254f2f3b5fe1e08a3475fd395
GwladysE <gengels@trimoz.com>
2023-04-18 18:07:34 -0400

    removed CV link since I have several versions (Fr/En Eur/Can))

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -2,7 +2,6 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from "../AnimatedLetters";
 import { useEffect, useState } from "react";
-import Pdf from './Gwladys_Engels_Feb_2023.pdf';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -36,9 +35,6 @@ const Experience = () => {
             />
         </h1>
         <div className='content-zone'>
-          <div className="Download-Resume">
-              <button><a href = {Pdf} target = "_blank" rel="noreferrer">See My Resume</a></button>
-          </div>
           <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/gwladysengels/' className='Linkedin-bck'>
                 <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
           </a>
